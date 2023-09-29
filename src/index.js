7833b4c4a26f1317531e660fcd171047225561db
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-09-29 15:43:20 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+const server = http.createServer((request, response )=> {
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end('<h1>Hello word!</h1>')
+
+})
+
+server.listen(3300, ()=> console.log('🔥 server started at http://localhost:3300'))  
