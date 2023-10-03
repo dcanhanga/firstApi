879f7adb9f652119eef3c463088e24f9e08d673c
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-10-03 18:31:15 +0100

    feat: :fire: First route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-const http = require('http')
-
-const server = http.createServer((request, response )=> {
-  response.writeHead(200, {'Content-Type': 'text/html'})
-  response.end('<h1>Hello word!</h1>')
-
+const http = require('http');
+const users = require('./mocks/users')
+const server = http.createServer((request, response)=> {
+ 
+ 
+  if(request.url === '/users' && request.method === 'GET'){
+    response.writeHead(200, {'Content': 'application/json'});
+    response.end(JSON.stringify(users))
+  } else {
+    response.writeHead(404, 'text/html')
+    response.end(`<h1>Cannot Found ${request.method} ${request.url}</h1>`)
+  }
 })
 
-server.listen(3300, ()=> console.log('🔥 server started at http://localhost:3300'))  
+server.listen(3333, ()=> console.log('🔥Server in run in http://localhost:3333'));
---
+++ src/mocks/users.js
@@ -0,0 +1,9 @@
+module.exports = [{
+  id:1,
+  name: "Domingos"
+}
+,
+{
+  id:2,
+  name:"Zinga"
+}]
